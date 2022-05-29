a58c5a934815f75360cbe1406e6791dac91ddcba
Niva Razin <nivarazin@Nivas-MacBook-Pro.local>
2022-05-29 08:56:04 -0500

    added all dropdowns

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ctecs = require('./ctecs.json');
 
 const ranker = {
-  title: "CTEC Course Ranker"
+  title: "CTEC Course Ranker .... SeeCATS, not CTECs ;)"
 };
 
 const Course = ({ course }) => (
@@ -18,18 +18,83 @@ const CourseList = ({ courses }) => (
   </div>
 );
 
-const ProfOption = ({ course }) => (
-  <option value ={course.professor}>{course.professor}</option>
-);
+const ProfOption = (prof) => {
+  console.log(prof)
+  return (
+  <option value ={prof}>{prof}</option>
+  );
+};
 
-const ProfDropdown = ({ courses }) => (
+const ProfDropdown = ({ courses }) => {
+  let uniqueProfs = [...new Set(courses.map(ctec => ctec.professor))];
+  return (
   <select name="profs" id="profs">
-    { Object.values(courses).map(course => <ProfOption course={ course } />) }
-  </select>
-);
+    { uniqueProfs.map(ProfOption) }
+  </select>);
+};
 
-console.log(ctecs.filter(ctec => ctec.professor.includes("Aravindan")))
-console.log(ctecs.filter(ctec => ctec.course_number == 110))
+const DeptOption = (dept) => {
+  console.log(dept)
+  return (
+  <option value ={dept}>{dept}</option>
+  );
+};
+
+const DeptDropdown = ({ courses }) => {
+  let uniqueDepts = [...new Set(courses.map(ctec => ctec.department))];
+  return (
+  <select name="depts" id="depts">
+    { uniqueDepts.map(DeptOption) }
+  </select>);
+};
+
+const CourseNumOption = (course) => {
+  console.log(course)
+  return (
+  <option value ={course}>{course}</option>
+  );
+};
+
+const CourseNumDropdown = ({ courses }) => {
+  let uniqueCourses = [...new Set(courses.map(ctec => ctec.course_number))];
+  return (
+  <select name="course" id="course">
+    { uniqueCourses.map(CourseNumOption) }
+  </select>);
+};
+
+const QtrOption = (qtr) => {
+  console.log(qtr)
+  return (
+  <option value ={qtr}>{qtr}</option>
+  );
+};
+
+const QtrDropdown = ({ courses }) => {
+  let uniqueCourses = [...new Set(courses.map(ctec => ctec.quarter))];
+  return (
+  <select name="qtrs" id="qtrs">
+    { uniqueCourses.map(QtrOption) }
+  </select>);
+};
+
+const YearOption = (year) => {
+  console.log(year)
+  return (
+  <option value ={year}>{year}</option>
+  );
+};
+
+const YearDropdown = ({ courses }) => {
+  let uniqueCourses = [...new Set(courses.map(ctec => ctec.year))];
+  return (
+  <select name="years" id="years">
+    { uniqueCourses.map(YearOption) }
+  </select>);
+};
+
+// console.log(ctecs.filter(ctec => ctec.professor.includes("Aravindan")))
+// console.log(ctecs.filter(ctec => ctec.course_number == 110))
 
 
 const App = () =>  (
@@ -37,11 +102,19 @@ const App = () =>  (
     <h1>{ ranker.title }</h1>
     <CourseList courses={ ctecs } />
 
+    <div id="dropdowns">
     <form action="/action_page.php">
+      <DeptDropdown courses = {ctecs} />
+      <CourseNumDropdown courses = {ctecs} />
       <ProfDropdown courses = {ctecs} />
+      <QtrDropdown courses = {ctecs} />
+      <YearDropdown courses = {ctecs} />
+
+
       <br></br>
       <input type="submit" value="Submit" />
     </form>
+    </div>
 
   </div>
 
